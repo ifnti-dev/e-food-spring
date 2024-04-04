14c30397cd084cb534195540932e28569f3b9a5e
matheSOK <sokamarthe@gmail.com>
2024-04-04 13:41:47 +0000

    initialisation du projet spring-boot

---
+++ e-food/src/test/java/com/entreprise/efood/EFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entreprise.efood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
