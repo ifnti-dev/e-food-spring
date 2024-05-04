772a340a0d5ac4692f4dd10b6f80636e9c32e52a
marthe <marthe@example.com>
2024-05-04 16:29:02 +0000

    dao

---
+++ e-food/src/main/java/com/entreprise/efood/repository/RestaurantRepository.java
@@ -0,0 +1,23 @@
+package com.entreprise.efood.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.entreprise.efood.Models.Restaurant;
+import com.entreprise.efood.dtos.RestaurantDTO;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long>{
+
+    @Query("SELECT new com.entreprise.efood.dtos.RestaurantDTO(r.id, r.nom, r.ville, r.adresse, r.telephone, r.heure_ouverture, r.heure_fermeture, r.jour_ouverture , r.etat,r.coordonnee_gps_x,r.coordonnee_gps_y) FROM Restaurant r ")
+    public List<RestaurantDTO> getAllRestaurants();
+
+}
+
+
+  
+  
