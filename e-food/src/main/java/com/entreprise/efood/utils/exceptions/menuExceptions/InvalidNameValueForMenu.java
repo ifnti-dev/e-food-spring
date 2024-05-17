9c7ff9a4fd0a7fee857d12e3d45e5ca6cb685690
toureydaou <ttoureydaou@gmail.com>
2024-05-17 11:44:20 +0000

    adding menu exceptions

---
+++ e-food/src/main/java/com/entreprise/efood/utils/exceptions/menuExceptions/InvalidNameValueForMenu.java
@@ -0,0 +1,7 @@
+package com.entreprise.efood.utils.exceptions.menuExceptions;
+
+public class InvalidNameValueForMenu extends Exception {
+    public InvalidNameValueForMenu(String message) {
+        super(message);
+    }
+}
---
+++ e-food/src/main/java/com/entreprise/efood/utils/exceptions/menuExceptions/InvalidTempPreparationValueForMenu.java
@@ -0,0 +1,8 @@
+package com.entreprise.efood.utils.exceptions.menuExceptions;
+
+public class InvalidTempPreparationValueForMenu extends Exception {
+
+    public InvalidTempPreparationValueForMenu(String message) {
+        super(message);
+    }
+}
