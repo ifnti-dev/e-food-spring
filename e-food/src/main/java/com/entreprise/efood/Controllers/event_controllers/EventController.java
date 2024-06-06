df423c315688a4ad5a3324250c7344f8ad1142cf
toureydaou <ttoureydaou@gmail.com>
2024-06-06 09:33:23 +0000

    fix minor code bugs

--- e-food/src/main/java/com/entreprise/efood/Controllers/event_controllers/EventController.java
+++ e-food/src/main/java/com/entreprise/efood/Controllers/event_controllers/EventController.java
@@ -7,7 +7,6 @@ import com.entreprise.efood.dtos.EventDTO;
 import com.entreprise.efood.services.implEnvt.JsonEventRestaurantEvent;
 import com.entreprise.efood.utils.AppConstant;
 
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,52 +18,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
-
-
-
 @RestController
 @RequestMapping(path = AppConstant.EVENT_REST_PATH_API)
 public class EventController {
-   
+
     private JsonEventRestaurantEvent eventService;
 
-    public EventController(JsonEventRestaurantEvent eventService){
+    public EventController(JsonEventRestaurantEvent eventService) {
         this.eventService = eventService;
     }
 
-    @PostMapping(value="/")
+    @PostMapping(value = "/")
     public EventDTO createRestaurantEvents(@RequestBody EventDTO event) {
-        
+
         eventService.createEvent(event);
-        
+
         return event;
     }
 
-    @GetMapping(value="/{restaurant_id}")
+    @GetMapping(value = "/{restaurant_id}")
     public List<EventDTO> getRestaurantEvents(@PathVariable("restaurant_id") String restaurant_id) {
         return eventService.getEventsByRestaurant(restaurant_id);
     }
 
-    @DeleteMapping(value="/",produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public EventDTO requestDeleteEvent(@RequestBody EventDTO event) {
 
         eventService.deleteEvent(event);
         return event;
     }
 
-    @PutMapping(value = "/",produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public EventDTO updateRestaurantEvent(@RequestBody EventDTO event) {
-        //TODO: process PUT request
+        // TODO: process PUT request
         // eventService.updateEvent(event);
         eventService.updateEvent(event);
-        
+
         return event;
     }
-    
-    
-   
-    
+
 }
