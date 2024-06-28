654f9ff862d1bb4d47c419fb222530bad6eb1f88
fideleChester <ekloufidele82@gmail.com>
2024-06-28 14:58:25 +0000

    ...

---
+++ e-food/src/main/java/com/entreprise/efood/dtos/OrderCommandDTO.java
@@ -0,0 +1,11 @@
+package com.entreprise.efood.dtos;
+
+public class OrderCommandDTO {
+    private Long idCommand;
+    private Long idClient;
+    private String coordX;
+    private String coordY;
+    private String preferences;
+    //private int idLivreur;
+
+}
