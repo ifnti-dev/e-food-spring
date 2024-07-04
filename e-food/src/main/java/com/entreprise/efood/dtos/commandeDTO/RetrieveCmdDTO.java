1b21ffe2b35580769abe0d2e31239714484542fb
fideleChester <ekloufidele82@gmail.com>
2024-07-04 12:04:44 +0000

    add commands attirbutes

--- e-food/src/main/java/com/entreprise/efood/dtos/commandeDTO/RetrieveCmdDTO.java
+++ e-food/src/main/java/com/entreprise/efood/dtos/commandeDTO/RetrieveCmdDTO.java
@@ -1,6 +1,9 @@
 package com.entreprise.efood.dtos.commandeDTO;
 
+import java.sql.Timestamp;
+
 import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,17 +11,19 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RetrieveCmdDTO {
     @NotNull
     private double montant;
     @NotNull
     private Long id;
+    @NotNull
+    private Timestamp date;
+    @NotNull
+    private String nom;
+    @NotNull
+    private String prenom;
 
-  
 
 
-    public RetrieveCmdDTO(Long id, double mnt) {
-        this.id = id;
-        this.montant = mnt;
-     }
 }
